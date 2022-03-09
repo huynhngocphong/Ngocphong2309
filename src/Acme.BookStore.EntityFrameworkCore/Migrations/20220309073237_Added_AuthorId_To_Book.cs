abe5f9edc43cc8abc1e398b8ac94012a831eb136
Ngocphong <huynhngocphong239@gmail.com>
2022-03-09 14:56:22 +0700

    first commit

---
+++ src/Acme.BookStore.EntityFrameworkCore/Migrations/20220309073237_Added_AuthorId_To_Book.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acme.BookStore.Migrations
+{
+    public partial class Added_AuthorId_To_Book : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AuthorId",
+                table: "AppBooks",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "AppAuthors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ShortBio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExtraProperties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    DeleterId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppAuthors", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppBooks_AuthorId",
+                table: "AppBooks",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppAuthors_Name",
+                table: "AppAuthors",
+                column: "Name");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppBooks_AppAuthors_AuthorId",
+                table: "AppBooks",
+                column: "AuthorId",
+                principalTable: "AppAuthors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppBooks_AppAuthors_AuthorId",
+                table: "AppBooks");
+
+            migrationBuilder.DropTable(
+                name: "AppAuthors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppBooks_AuthorId",
+                table: "AppBooks");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "AppBooks");
+        }
+    }
+}
